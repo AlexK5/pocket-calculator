e87bab578a0c6b1eee0b7c39fe9f6ad7dc99d795
Alex Keri <akeri@ucvts.org>
2019-01-03 10:50:54 -0500

    worked on commas

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,21 +1,36 @@
-function numPress(number){
+let display = [0];
+let display2=[0];
+let value = "";
+function numPress(y){
+  let number=String(y)
   let p = document.getElementById("display");
   let list=[];
+  let numLength;
   if(p.innerHTML=="0"){
     p.innerHTML="";
+    display=[];
+    display2=[]
   }
-  p.innerHTML+=number;
-  for(let i = 0; i<p.innerHTML.length; i++){
-    if(p.innerHTML[i]!=","){
-      if(list.length%4==3){
-        list.push(",");
-      }
-      list.push(p.innerHTML[i]);
+  display=display2
+  display.push(number);
+  display2=display;
+  value+=number;
+  numLength+=1;
+  for(let i = 0; i<display.length; i++){
+    if(display[i]==","){
+      display.splice(i,1);
     }
   }
-  p.innerHTML="";
-  let j;
-  for(j in list){
-    p.innerHTML+=list[j];
+  for(let j = 0; j<display.length; j++){
+    if((numLength-j)%3==0 && numLength>j){
+      display.splice(j,0,",");
+    }
   }
+  display.push("");
+  let x = display.length;
+  for(let k = 0; k<x-1; k++){
+    display[x-1]+=display[k];
+    display=display[x-1];
+}
+  p.innerHTML=display;
 }
